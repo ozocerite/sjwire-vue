b8ca2d35031e748cb25ef043373b39112d956d95
hklee <ozocerite@gmail.com>
2022-10-10 03:28:10 +0900

    init

--- src/views/CompanyContact.vue
+++ src/views/CompanyContact.vue
@@ -15,7 +15,16 @@
     <div class="container-fluid bg-light overflow-hidden px-lg-0" style="margin: 3rem 0;">
       <div class="container contact px-lg-0">
         <div class="row g-0 mx-lg-0">
-          <div class="col-lg-5 contact-text py-5 wow fadeIn" data-wow-delay="0.5s" style="padding-left: 8%;">
+          <div class="col-lg-7 pe-lg-0" style="min-height: 400px;">
+            <div class="position-relative h-100">
+              <iframe class="position-absolute w-100 h-100" style="object-fit: cover;"
+                      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3179.4135460577095!2d126.8931056156545!3d37.166641954685275!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357b14c6abb18351%3A0xe600b90888983a6e!2z7IS47KeE7LKg66ed!5e0!3m2!1sko!2skr!4v1665249411502!5m2!1sko!2skr"
+                      frameborder="0" allowfullscreen="" aria-hidden="false"
+                      tabindex="0"/>
+            </div>
+          </div>
+
+          <div class="col-lg-5 contact-text py-5 wow fadeIn" data-wow-delay="0.5s" style="padding-left: 0;">
             <div class="p-lg-5 ps-lg-0">
 <!--              주소-->
               <div class="text-center mx-auto mt-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 600px;">
@@ -73,14 +82,7 @@
               </div>
             </div>
           </div>
-          <div class="col-lg-7 pe-lg-0" style="min-height: 400px;">
-            <div class="position-relative h-100">
-              <iframe class="position-absolute w-100 h-100" style="object-fit: cover;"
-                      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3179.4135460577095!2d126.8931056156545!3d37.166641954685275!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357b14c6abb18351%3A0xe600b90888983a6e!2z7IS47KeE7LKg66ed!5e0!3m2!1sko!2skr!4v1665249411502!5m2!1sko!2skr"
-                      frameborder="0" allowfullscreen="" aria-hidden="false"
-                      tabindex="0"/>
-            </div>
-          </div>
+
         </div>
       </div>
     </div>
