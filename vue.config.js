99c60e865dfb9c31c69c08fec7eef254b6cd77fa
hyeonkyeong lee <ozocerite@gmail.com>
2022-10-09 23:05:57 +0900

    init project

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,8 @@ const { defineConfig } = require('@vue/cli-service');
 const webpack = require("webpack");
 
 module.exports = defineConfig({
+  outputDir: './docs',
+  publicPath: process.env.NODE_ENV === 'production'?'/sjwire-vue':'/',
   transpileDependencies: true,
   lintOnSave: false,
   configureWebpack: {
